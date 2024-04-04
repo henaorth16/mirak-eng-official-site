cb1da3cb494f4e4f0ee2291a69928458184a6c95
JoFlut <131449224+yoniflutter@users.noreply.github.com>
2024-04-04 19:35:58 +0300

    Update TrustedBy.jsx

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -13,7 +13,8 @@ import part5 from "../assets/icons/part5.jpg"
 import part6 from "../assets/icons/part6.jpg"
 import part7 from "../assets/icons/part7.jpg"
 import part8 from "../assets/icons/part8.png"
-const imageList = [part1, part2, part3, part4, part5, part6, part7, part8]
+import part9 from "../assets/icons/part9.jpg"
+const imageList = [part1, part2, part3, part4, part5, part6, part7, part8, part9]
 function TrustedBy() {
     register()
     return (
@@ -21,7 +22,7 @@ function TrustedBy() {
             <h1 className="text-4xl text-center font-bold mb-6">Trusted by</h1>
             <Swiper
                 modules={[EffectCoverflow, Autoplay, Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={1}
+                spaceBetween={3}
                 slidesPerView={7}
                 // effect="coverflow"
                 autoplay={{
@@ -34,7 +35,7 @@ function TrustedBy() {
             >
                 {imageList.map(item => (
                     <SwiperSlide key={item}>
-                        <div className='w-[120px] overflow-hidden'>
+                        <div className='w-[120px] overflow-hidden  flex flex-col gap-2'>
                             <img className=' object-contain grayscale-[1] bg-blend-screen' src={item} alt="logo" />
                         </div>
                     </SwiperSlide>
@@ -43,70 +44,7 @@ function TrustedBy() {
             </Swiper>
         </div>
     )
-=======
-import { register } from "swiper/element/bundle";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import {
-  EffectCoverflow,
-  Autoplay,
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-} from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import part1 from "../assets/icons/part1.png";
-import part2 from "../assets/icons/part2.jpg";
-import part3 from "../assets/icons/part3.png";
-import part4 from "../assets/icons/part4.jpg";
-import part5 from "../assets/icons/part5.jpg";
-import part6 from "../assets/icons/part6.png";
-import part7 from "../assets/icons/part7.png";
-import part8 from "../assets/icons/part8.png";
-const imageList = [part1, part2, part3, part4, part5, part6, part7, part8];
-function TrustedBy() {
-  register();
-  return (
-    <div id="clients">
-      <h1 className="text-4xl text-center font-bold mb-6">Trusted by</h1>
-      <Swiper
-        modules={[
-          EffectCoverflow,
-          Autoplay,
-          Navigation,
-          Pagination,
-          Scrollbar,
-          A11y,
-        ]}
-        spaceBetween={1}
-        slidesPerView={5}
-        // effect="coverflow"
-        autoplay={{
-          // delay: 1500,
-          disableOnInteraction: false,
-          waitForTransition: true,
-        }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        {imageList.map((item) => (
-          <SwiperSlide key={item}>
-            <div className="w-[100px] overflow-hidden">
-              <img
-                className=" object-contain grayscale-[1] bg-blend-screen"
-                src={item}
-                alt="logo"
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
->>>>>>> 861a47cf6509a4d25c0a0dc72f1906a1e2703f4b
+
 }
 
 export default TrustedBy;
